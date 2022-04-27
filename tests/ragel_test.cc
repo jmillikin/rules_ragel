564af2b63b35dbc180b615fa3f59febfce07a605
John Millikin <john@john-millikin.com>
2022-04-27 11:14:25 +0900

    Use environment variables to locate test data.
    GitOrigin-RevId: 8bdeec98cbfa06e456a0f10ba2fb242871951a5f

--- tests/ragel_test.cc
+++ tests/ragel_test.cc
@@ -32,15 +32,40 @@ class RulesRagel : public ::testing::Test {
         return buf.str();
     }
 
+    string TestsDir() {
+        const char *test_binary_ptr = getenv("TEST_BINARY");
+        const char *test_workspace_ptr = getenv("TEST_WORKSPACE");
+
+        if (test_binary_ptr == nullptr) {
+            EXPECT_NE(test_binary_ptr, nullptr);
+            return "";
+        }
+        if (test_workspace_ptr == nullptr) {
+            EXPECT_NE(test_workspace_ptr, nullptr);
+            return "";
+        }
+
+        string test_binary(test_binary_ptr);
+        string test_workspace(test_workspace_ptr);
+
+        size_t slash = test_binary.find_last_of('/');
+        if (slash == string::npos) {
+            EXPECT_NE(slash, string::npos);
+            return "";
+        }
+
+        return test_workspace + "/" + test_binary.substr(0, slash);
+    }
+
     std::unique_ptr<Runfiles> runfiles_;
 };
 
 TEST_F(RulesRagel, GenruleCxx) {
-    const auto parser_src = ReadFile("rules_ragel/tests/genrule_output.cc");
+    const auto parser_src = ReadFile(TestsDir() + "/genrule_output.cc");
     ASSERT_THAT(parser_src, HasSubstr("static const int hello_world_start"));
 }
 
 TEST_F(RulesRagel, CompiledParserCxx) {
-    const auto hello_cc_bin = ReadFile("rules_ragel/tests/hello_cc_bin" EXE);
+    const auto hello_cc_bin = ReadFile(TestsDir() + "/hello_cc_bin" EXE);
     ASSERT_TRUE(hello_cc_bin.size() > 0);
 }
